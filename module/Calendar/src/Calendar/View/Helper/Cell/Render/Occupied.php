6a8b6a3c512653036523d337f754cc5619f6d0fc
maximilianilzhoefer <55856340+maximilianilzhoefer@users.noreply.github.com>
2021-11-27 20:37:16 +0100

    Update Occupied.php
    Berechtigungen geändert.

--- module/Calendar/src/Calendar/View/Helper/Cell/Render/Occupied.php
+++ module/Calendar/src/Calendar/View/Helper/Cell/Render/Occupied.php
@@ -13,7 +13,24 @@ class Occupied extends AbstractHelper
         $view = $this->getView();
 
         if ($user && $user->can('calendar.see-data')) {
-            return $view->calendarCellRenderOccupiedForPrivileged($reservations, $cellLinkParams);
+			return $view->calendarCellRenderOccupiedForPrivileged($reservations, $cellLinkParams);
+        }else if ($user && $user->can('calendar.see-past')){
+            if ($userBooking) {
+                $cellLabel = $view->t('Your Booking');
+                $cellGroup = ' cc-group-' . $userBooking->need('bid');
+
+                return $view->calendarCellLink($cellLabel, $view->url('square', [], $cellLinkParams), 'cc-own' . $cellGroup);
+            } else{
+				
+				
+				$reservation = current($reservations);
+				$booking = $reservation->needExtra('booking');
+				$uid = $booking->get('uid');
+				$cellLabel = $reservation->getExtra('booking')->getExtra('user')->get('alias');
+				#$cellLabel = $view->t('Your Booking');
+                $cellGroup = ' cc-group-' . $booking->need('bid');
+
+                return $view->calendarCellLink($cellLabel, $view->url('square', [], $cellLinkParams), 'cc-single' . $cellGroup);}
         } else if ($user) {
             if ($userBooking) {
                 $cellLabel = $view->t('Your Booking');
